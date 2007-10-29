01b1d70204d3afd8a2f97fd8dc89034d14715d09
Patrick <Patrick@542498e7-56f8-f544-a0f2-a85913ac812a>
2007-10-29 14:53:36 +0000

    TK-06075: Mercury QTP Tracer bullet (Test Suite counts only).

--- Services/FolderProcessorService.cs
+++ Services/FolderProcessorService.cs
@@ -7,16 +7,16 @@ using VersionOne.ServiceHost.Logging;
 
 namespace VersionOne.ServiceHost.Core
 {
-	public abstract class FolderProcessorService : IHostedService
+	public abstract class FileProcessorService : IHostedService
 	{
 		private IEventManager _eventmanager;		
-		private FolderMonitor _monitor;
+		private FileMonitor _monitor;
 
 		protected IEventManager EventManager { get { return _eventmanager; } }
 		
 		public void Initialize(XmlElement config, IEventManager eventManager, IProfile profile)
 		{
-			_monitor = new FolderMonitor(profile, config["Watch"].InnerText, config["Filter"].InnerText, Process);
+			_monitor = new FileMonitor(profile, config["Watch"].InnerText, config["Filter"].InnerText, Process);
 			_eventmanager = eventManager;			
 			_eventmanager.Subscribe(EventSinkType, _monitor.ProcessFolder);
 		}
@@ -36,6 +36,40 @@ namespace VersionOne.ServiceHost.Core
 		}
 		
 		protected abstract void InternalProcess(string file);
+		protected abstract Type EventSinkType { get; }
+	}
+
+	public abstract class FolderBatchProcessorService : IHostedService
+	{
+		private IEventManager _eventmanager;
+		private BatchFolderMonitor _monitor;
+		private string _folderFilterPattern;
+
+		protected IEventManager EventManager { get { return _eventmanager; } }
+
+		public void Initialize(XmlElement config, IEventManager eventManager, IProfile profile)
+		{
+			_folderFilterPattern = config["Filter"].InnerText;
+			_monitor = new BatchFolderMonitor(profile, config["Watch"].InnerText, _folderFilterPattern, Process);
+			_eventmanager = eventManager;
+			_eventmanager.Subscribe(EventSinkType, _monitor.ProcessFolder);
+		}
+
+		private void Process(string[] folders)
+		{
+			try
+			{
+				//LogMessage.Log(string.Format("Starting Processing Folder: {0}", folder), EventManager);
+				InternalProcess(folders);
+				//LogMessage.Log(string.Format("Finished Processing Folder: {0}", folder), EventManager);
+			}
+			catch (Exception ex)
+			{
+				//LogMessage.Log(string.Format("Failed Processing Folder: {0}", folder), ex, EventManager);
+			}
+		}
+
+		protected abstract void InternalProcess(string[] folders);
 		protected abstract Type EventSinkType { get; }
 	}
 }
--- Utility/FolderMonitor.cs
+++ Utility/FolderMonitor.cs
@@ -1,69 +1,165 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using VersionOne.Profile;
 
 namespace VersionOne.ServiceHost.Core
 {
-	public delegate void ProcessFileDelegate(string file);	
-	public class FolderMonitor
+	public delegate void ProcessFileDelegate(string file);
+	public delegate void ProcessFolderDelegate(string folder);
+	public delegate void ProcessFolderBatchDelegate(string[] folders);
+
+	public abstract class FileSystemMonitor
 	{
 		private IProfile _profile;
-		private IProfile _processfilesprofile;
-		private string _watchfolder;
+		private IProfile _processedPathsProfile;
 		private string _filterpattern;
-		private ProcessFileDelegate _processor;
+		private string _watchfolder;
 
-		private IProfile ProcessedFiles
+		private IProfile ProcessedPaths
 		{
 			get
 			{
-				if (_processfilesprofile == null)
-					_processfilesprofile = _profile["ProcessedFiles"];
-				return _processfilesprofile;
-			}
-		}			
-		public FolderMonitor(IProfile profile, string watchfolder, string filterpattern, ProcessFileDelegate processor)
-		{
-			_profile = profile;			
-			_watchfolder = watchfolder;
-			_filterpattern = filterpattern;
-			_processor = processor;
-			
-			if (string.IsNullOrEmpty(_filterpattern))
-				_filterpattern = "*.*";
-			string path = Path.GetFullPath(_watchfolder);
-			if (!Directory.Exists(path))
-				Directory.CreateDirectory(path);						
-		}
-
-		public void ProcessFolder(object pubobj)
-		{
-			string path = Path.GetFullPath(_watchfolder);
-			string[] files = Directory.GetFiles(path, _filterpattern);
-			foreach (string file in files)
-				ProcessFile(file);
-		}
-
-		private void ProcessFile(string file)
-		{
-			if (GetState(file) == null)
-			{
-				SaveState(file, false);
-				_processor(file);
-				SaveState(file, true);
+				if (_processedPathsProfile == null)
+					_processedPathsProfile = _profile["ProcessedFiles"]; // Retaining name "ProcessedFiles" for backward-compatibility
+				return _processedPathsProfile;
 			}
 		}
 
-		private bool? GetState(string file)
+		protected string FilterPattern
 		{
-			string value = ProcessedFiles[file].Value;
+			get { return _filterpattern; }
+			set { _filterpattern = value; }
+		}
+
+		protected string WatchFolder
+		{
+			get { return _watchfolder; }
+			set { _watchfolder = value; }
+		}
+
+		protected bool? GetState(string file)
+		{
+			string value = ProcessedPaths[file].Value;
 			if (value == null)
 				return null;
 			return bool.Parse(value);
 		}
-		
-		private void SaveState(string file, bool? done)
+
+		protected void SaveState(string file, bool? done)
 		{
-			ProcessedFiles[file].Value = done == null ? null : done.ToString();
+			ProcessedPaths[file].Value = done == null ? null : done.ToString();
+		}
+
+		public FileSystemMonitor(IProfile profile, string watchfolder, string filterpattern)
+		{
+			_profile = profile;
+			WatchFolder = watchfolder;
+			FilterPattern = filterpattern;
+			if (string.IsNullOrEmpty(FilterPattern))
+				FilterPattern = "*.*";
+
+			string path = Path.GetFullPath(WatchFolder);
+			if (!Directory.Exists(path))
+				Directory.CreateDirectory(path);
+		}
+
+		protected void ProcessPath(string path)
+		{
+			if (GetState(path) == null)
+			{
+				SaveState(path, false);
+				InvokeProcessor(path);
+				SaveState(path, true);
+			}
+		}
+
+		protected abstract void InvokeProcessor(string path);
+	}
+
+	public class FileMonitor : FileSystemMonitor
+	{
+		private ProcessFileDelegate _processor;
+
+		public FileMonitor(IProfile profile, string watchfolder, string filterpattern, ProcessFileDelegate processor)
+			: base(profile, watchfolder, filterpattern)
+		{
+			_processor = processor;			
+		}
+
+		public void ProcessFolder(object pubobj)
+		{
+			string path = Path.GetFullPath(WatchFolder);
+			string[] files = Directory.GetFiles(path, FilterPattern);
+			foreach (string file in files)
+				ProcessPath(file);
+		}
+
+		protected override void InvokeProcessor(string path)
+		{
+			_processor(path);
+		}
+	}
+
+	public class FolderMonitor : FileSystemMonitor
+	{
+		private ProcessFolderDelegate _processor;
+
+		public FolderMonitor(IProfile profile, string watchfolder, string filterpattern, ProcessFolderDelegate processor)
+			: base(profile, watchfolder, filterpattern)
+		{
+			_processor = processor;
+		}
+
+		public void ProcessFolder(object pubobj)
+		{
+			string path = Path.GetFullPath(WatchFolder);
+			string[] subFolders = Directory.GetDirectories(path, FilterPattern);
+			foreach (string subFolder in subFolders)
+				ProcessPath(subFolder);
+		}
+
+		protected override void InvokeProcessor(string path)
+		{
+			_processor(path);
+		}
+	}
+
+	public class BatchFolderMonitor : FileSystemMonitor
+	{
+		private ProcessFolderBatchDelegate _processor;
+		public BatchFolderMonitor(IProfile profile, string watchfolder, string filterpattern, ProcessFolderBatchDelegate processor) : base(profile, watchfolder, filterpattern)
+		{
+			_processor = processor;
+		}
+
+		public void ProcessFolder(object pubobj)
+		{
+			string path = Path.GetFullPath(WatchFolder);
+			string[] subFolders = Directory.GetDirectories(path, FilterPattern, SearchOption.AllDirectories);
+
+			List <string> notProcessed = new List<string>();
+			foreach (string subFolder in subFolders)
+			{
+				if (GetState(subFolder) == null)
+					notProcessed.Add(subFolder);
+			}
+
+			if (notProcessed.Count == 0)
+				return;
+
+			foreach(string subFolder in notProcessed)
+				SaveState(subFolder, false);
+
+			_processor(notProcessed.ToArray());
+
+			foreach (string subFolder in notProcessed)
+				SaveState(subFolder, true);
+		}
+
+		protected override void InvokeProcessor(string path)
+		{
+			// TODO: Fix this smell
 		}
 	}
 }
