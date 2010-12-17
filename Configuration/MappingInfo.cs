706ca0027499ff795b32cdbbda6f826d8486311a
Maxim <Maxim@542498e7-56f8-f544-a0f2-a85913ac812a>
2010-12-17 13:12:54 +0000

    Bugzilla project and priority mapping support added (strories B-01588, B-01589)

---
+++ Configuration/MappingInfo.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace VersionOne.ServiceHost.Core.Configuration
+{
+    [DebuggerDisplay("Mapping (Id={Id}, Name={Name})")]
+    public class MappingInfo
+    {
+        public readonly string Id;
+        public readonly string Name;
+
+        public MappingInfo(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != typeof(MappingInfo))
+            {
+                return false;
+            }
+
+            MappingInfo other = (MappingInfo)obj;
+            return string.Equals(Id, other.Id) && string.Equals(Name, other.Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
