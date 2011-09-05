0a14fc58c95777811b97e647150530b1a942c925
Mikhail <Mikhail@542498e7-56f8-f544-a0f2-a85913ac812a>
2011-09-05 08:42:05 +0000

    updated running startup validators for LKK and BaF

--- StartupValidation/StartupCheckerBase.cs
+++ StartupValidation/StartupCheckerBase.cs
@@ -3,15 +3,12 @@ using VersionOne.ServiceHost.Eventing;
 
 namespace VersionOne.ServiceHost.Core.StartupValidation {
     public abstract class StartupCheckerBase {
-        private readonly IEventManager eventManager;
-
         protected StartupCheckerBase(IEventManager eventManager) {
-            this.eventManager = eventManager;
             eventManager.Subscribe(typeof(ServiceHostState), Run);
         }
 
         public void Run(object obj) {
-            if (obj == null || !ServiceHostState.Validate.Equals(obj)) {
+            if(obj == null || !ServiceHostState.Validate.Equals(obj)) {
                 return;
             }
 
@@ -20,10 +17,6 @@ namespace VersionOne.ServiceHost.Core.StartupValidation {
             foreach(var step in steps) {
                 step.Run();
             }
-        }
-
-        private void Shutdown() {
-            eventManager.Publish(ServiceHostState.Shutdown);
         }
 
         protected abstract ReadOnlyCollection<IValidationStep> CreateValidators();
