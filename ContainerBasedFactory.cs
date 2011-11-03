eb036bc4fe5f1002972982e4a7449b00a3aa254d
Eugene <Eugene@542498e7-56f8-f544-a0f2-a85913ac812a>
2011-11-03 13:00:01 +0000

    IoC container based factories - currently stubs

---
+++ ContainerBasedFactory.cs
@@ -0,0 +1,16 @@
+﻿using Ninject;
+using Ninject.Parameters;
+
+namespace VersionOne.ServiceHost.Core {
+    public abstract class ContainerBasedFactory {
+        protected readonly IKernel Container;
+
+        protected ContainerBasedFactory(IKernel container) {
+            Container = container;
+        }
+
+        protected IParameter Parameter(string name, object value) {
+            return new ConstructorArgument(name, value);
+        }
+    }
+}
