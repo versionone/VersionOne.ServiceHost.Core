0628ef1249e9405c6c82f963ec0773ee3476d082
Mikhail <Mikhail@542498e7-56f8-f544-a0f2-a85913ac812a>
2011-11-03 16:04:47 +0000

    added draft permission processing added new validation step

---
+++ StartupValidation/NonStrictValidationSimpleStep.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VersionOne.ServiceHost.Core.StartupValidation {
+    public class NonStrictValidationSimpleStep: IValidationStep {
+        private readonly ISimpleResolver resolver;
+        private readonly ISimpleValidator validator;
+
+        public NonStrictValidationSimpleStep(ISimpleValidator validator, ISimpleResolver resolver) {
+            this.validator = validator;
+            this.resolver = resolver;
+        }
+
+        public void Run() {
+            if(validator == null) {
+                throw new InvalidOperationException("Cannot run the step without a validator.");
+            }
+            var isValid = validator.Validate();
+
+            if(!isValid && resolver != null){
+                resolver.Resolve();
+            }
+        }
+    }
+}
