0b53be55ae0076c0ffd038406a6cb7e6b9d13309
Eugene <Eugene@542498e7-56f8-f544-a0f2-a85913ac812a>
2011-12-21 12:28:55 +0000

    relaxed ILogger dependency of VersionOneProcessor so it could be reused externally

--- Logging/LogMessage.cs
+++ Logging/LogMessage.cs
@@ -1,28 +1,23 @@
 using System;
-using VersionOne.ServiceHost.Eventing;
 
-namespace VersionOne.ServiceHost.Core.Logging
-{
-	public class LogMessage
-	{
-		public enum SeverityType
-		{
-			Debug,			
-			Info,
-			Error
-		}
+namespace VersionOne.ServiceHost.Core.Logging {
+    public class LogMessage {
+        public enum SeverityType {
+            Debug,
+            Info,
+            Error
+        }
 
-		public readonly SeverityType Severity;
-		public readonly string Message;
-		public readonly Exception Exception;
-		public readonly DateTime Stamp;
-		
-		public LogMessage(SeverityType severity, string message, Exception exception)
-		{
-			Severity = severity;
-			Message = message;
-			Exception = exception;
-			Stamp = DateTime.Now;
-		}
-	}
+        public readonly SeverityType Severity;
+        public readonly string Message;
+        public readonly Exception Exception;
+        public readonly DateTime Stamp;
+
+        public LogMessage(SeverityType severity, string message, Exception exception) {
+            Severity = severity;
+            Message = message;
+            Exception = exception;
+            Stamp = DateTime.Now;
+        }
+    }
 }
