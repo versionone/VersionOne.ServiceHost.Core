d0d2f588771f50473aff78b5af4bf1749153e14f
Mikhail <Mikhail@542498e7-56f8-f544-a0f2-a85913ac812a>
2012-04-09 10:15:28 +0000

    updated TestService now it get AffectedDefects not from test parent by request separately refactored to .Net 3.5 updated tests

--- Services/V1WriterServiceBase.cs
+++ Services/V1WriterServiceBase.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Xml;
-using Ninject;
 using VersionOne.SDK.APIClient;
 using VersionOne.ServiceHost.Eventing;
 using VersionOne.Profile;
