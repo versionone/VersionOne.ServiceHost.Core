d061c680fd488821364efb01829a4827c2c32be4
Joe Koberg <joe.koberg@versionone.com>
2014-01-02 12:05:08 -0500

    Use newer infrastructure from build tools

--- build.sh
+++ build.sh
@@ -14,111 +14,24 @@ cd "$WORKSPACE"
 export WORKSPACE=`pwd`
 
 
-
-# ----- Utility functions -----------------------------------------------------
-
-function winpath() {
-  # Convert gitbash style path '/c/Users/Big John/Development' to 'c:\Users\Big John\Development',
-  # via dumb substitution. Handles drive letters; incurs process creation penalty for sed.
-  if [ -e /etc/bash.bashrc ] ; then
-    # Cygwin specific settings
-    echo "`cygpath -w $1`"
-  else
-    # Msysgit specific settings
-    echo "$1" | sed -e 's|^/\(\w\)/|\1:\\|g;s|/|\\|g'
+# ----- Common ----------------------------------------------------------------
+# Common build script creates functions and variables expected by Jenkins.
+if [ -d $WORKSPACE/../build-tools ]; then
+  ## When script directory already exists, just update when there are changes.
+  cd $WORKSPACE/../build-tools
+  git fetch && git stash
+  if ! git log HEAD..origin/master --oneline --quiet; then
+    git pull
   fi
-}
-
-function bashpath() {
-  # Convert windows style path 'c:\Users\Big John\Development' to '/c/Users/Big John/Development'
-  # via dumb substitution. Handles drive letters; incurs process creation penalty for sed.
-  if [ -e /etc/bash.bashrc ] ; then
-    # Cygwin specific settings
-    echo "`cygpath $1`"
-  else
-    # Msysgit specific settings
-    echo "$1" | sed -e 's|\(\w\):|/\1|g;s|\\|/|g'
-  fi
-}
-
-function parentwith() {  # used to find $WORKSPACE, below.
-  # Starting at the current dir and progressing up the ancestors,
-  # retuns the first dir containing $1. If not found returns pwd.
-  SEARCHTERM="$1"
-  DIR=`pwd`
-  while [ ! -e "$DIR/$SEARCHTERM" ]; do
-    NEWDIR=`dirname "$DIR"`
-    if [ "$NEWDIR" = "$DIR" ]; then
-      pwd
-      return
-    fi
-    DIR="$NEWDIR"
-  done
-  echo "$DIR"
-  }
-
-
-# If we aren't running under jenkins. some variables will be unset.
-# So set them to a reasonable value
-
-if [ -z "$WORKSPACE" ]; then
-  export WORKSPACE=`parentwith .git`;
-fi
-
-TOOLSDIRS=". $WORKSPACE/GetBuildTools $WORKSPACE/v1_build_tools $WORKSPACE/../v1_build_tools $WORKSPACE/nuget_tools"
-#TOOLSDIRS="."
-for D in $TOOLSDIRS; do
-  if [ -d "$D/bin" ]; then
-    export BUILDTOOLS_PATH="$D/bin"
-  fi
-done
-
-echo $(which $BUILDTOOLS_PATH/NuGet.exe)
-echo $(which $WORKSPACE/.nuget/NuGet.exe)
-
-if [ ! $(which $BUILDTOOLS_PATH/NuGet.exe) ] && [ -d "$WORKSPACE/.nuget" ] && [ ! $(which "$WORKSPACE/.nuget/nuget.exe") ]; then
-  # Get the latest nuget.exe
-  echo "Build is downloading the latest nuget.exe"
-  powershell -NoProfile -ExecutionPolicy unrestricted -Command "(new-object System.Net.WebClient).Downloadfile('http://nuget.org/nuget.exe', './.nuget/nuget.exe')"
-fi
-
-if [ ! $(which $BUILDTOOLS_PATH/NuGet.exe) ] && [ $(which $WORKSPACE/.nuget/NuGet.exe) ]; then
-  export BUILDTOOLS_PATH="$WORKSPACE/.nuget"
-fi
-echo "Using $BUILDTOOLS_PATH for NuGet"
-
-if [ -z "$DOTNET_PATH" ]; then
-  for D in `bashpath "$SYSTEMROOT\\Microsoft.NET\\Framework\\*"`; do
-    if [ -d $D ]; then
-      export DOTNET_PATH="$D"
-    fi
-  done
-fi
-echo "Using $DOTNET_PATH for .NET"
-
-export PATH="$PATH:$BUILDTOOLS_PATH:$DOTNET_PATH"
-
-if [ -z "$SIGNING_KEY_DIR" ]; then
-  export SIGNING_KEY_DIR=`pwd`;
-fi
-
-export SIGNING_KEY="$SIGNING_KEY_DIR/VersionOne.snk"
-
-if [ -f "$SIGNING_KEY" ]; then 
-  export SIGN_ASSEMBLY="true"
+  cd $WORKSPACE
 else
-  export SIGN_ASSEMBLY="false"
-  echo "Please place VersionOne.snk in `pwd` or $SIGNING_KEY_DIR to enable signing.";
+  git clone https://github.com/versionone/openAgile-build-tools.git $WORKSPACE/../build-tools
 fi
+source $WORKSPACE/../build-tools/common.sh
 
-if [ -z "$VERSION_NUMBER" ]; then
-  export VERSION_NUMBER="0.0.0"
-fi
 
-if [ -z "$BUILD_NUMBER" ]; then
-  # presume local workstation, use date-based build number
-  export BUILD_NUMBER=`date +%H%M`  # hour + minute
-fi
+
+
 
 # ---- Produce .NET Metadata -------------------------------------------------
 
@@ -161,11 +74,9 @@ MSBuild.exe $SOLUTION_FILE -m \
 
 # ---- Restore packages and update them to the latest compatible versions -----
 
-echo "Build is restoring NuGet packages"
-nuget restore $SOLUTION_FILE -Source $NUGET_FETCH_URL
 
-echo "Build is updating NuGet packages to latest compatible versions"
-nuget update $SOLUTION_FILE -Source $NUGET_FETCH_URL
+nuget_packages_refresh
+
 
 # ---- Build solution using msbuild -------------------------------------------
 
@@ -183,7 +94,7 @@ MSBuild.exe $SOLUTION_FILE \
 
 unset Platform
 cd $WORKSPACE/$MAIN_DIR
-NuGet.exe pack $MAIN_CSPROJ \
+../.nuget/NuGet.exe pack $MAIN_CSPROJ \
   -Symbols \
   -Properties Configuration="$Configuration"
 cd $WORKSPACE
